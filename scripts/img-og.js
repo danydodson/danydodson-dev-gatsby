d85004f54abd23ec3325b7aa6f21d5598b698834
Danydodson <danydodson@gmail.com>
2021-09-28 06:59:25 -0500

    updates

--- scripts/img-og.js
+++ scripts/img-og.js
@@ -1,21 +1,15 @@
-//@ts-check
 const nodeHtmlToImage = require('node-html-to-image')
 const uuid = require('uuid')
-const { format, parseJSON } = require('date-fns');
+const { format, parseJSON } = require('date-fns')
 
 const html = `
 <html>
   <head>
     <style>
-      body {
-        width: 1128px;
-        height: 600px;
-      }
+      body {width: 1128px;height: 600px;}
     </style>
-
     <!-- Include external CSS, JavaScript or Fonts! -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
     <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
   </head>
   <body>
@@ -24,15 +18,12 @@ const html = `
         <span class="tweet-text mb-2" style="font-size: 52px;">
           {title}
         </span>
-
         <span class="text-muted mb-2">
           {date}
         </span>
-
         <div class="flex justify-center my-4">
           <div class="rounded-full inline-flex" style="background-color: #198c95; height: 0.25rem; width: 4rem;"></div>
         </div>
-
         <div class="mb-2">
           <img src="https://www.eliostruyf.com/images/elio-dark-2019.png" class="rounded-circle" width="150px">
         </div>
@@ -42,24 +33,24 @@ const html = `
     </div>
   </body>
 </html>
-`;
+`
 
-const arguments = process.argv;
+const args = process.argv
 
-if (arguments && arguments.length > 0 && arguments[2]) {
-  const workspaceArg = arguments[2];
-  const fileArg = arguments[3];
-  const dataArg = arguments[4];
-  const data = dataArg && typeof dataArg === "string" ? JSON.parse(dataArg) : null;
-  
+if (args && args.length > 0 && args[2]) {
+
+  const workspaceArg = args[2]
+  const fileArg = args[3]
+  const dataArg = args[4]
+  const data = dataArg && typeof dataArg === "string" ? JSON.parse(dataArg) : null
+
   if (data.title && data.date) {
-    const parsedHtml = html.replace(`{title}`, data.title).replace(`{date}`, format(parseJSON(data.date), "MMM dd, yyyy"));
-    const fileName = `${uuid.v4()}.png`;
+    const parsedHtml = html.replace(`{title}`, data.title).replace(`{date}`, format(parseJSON(data.date), "MMM dd, yyyy"))
+    const fileName = `${uuid.v4()}.png`
 
-    // @ts-ignore
     nodeHtmlToImage({
       output: `${workspaceArg}/static/social/${fileName}`,
       html: parsedHtml
-    }).then(() => console.log(`preview: "/social/${fileName}"`)).catch(e => console.log(e?.message || e));
+    }).then(() => console.log(`preview: "/social/${fileName}"`)).catch(e => console.log(e?.message || e))
   }
 }

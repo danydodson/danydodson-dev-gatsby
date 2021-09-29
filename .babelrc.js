fce99262e4abcfd77ac6e17f356c3c96a282d312
Dany Dodson <danydodson@gmail.com>
2021-09-29 18:58:05 -0500

    up

--- .babelrc.js
+++ .babelrc.js
@@ -1,18 +1,4 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env'],
-    ['@babel/preset-react'],
-    ['babel-preset-gatsby']
-  ],
-  plugins: [
-    'babel-plugin-styled-components',
-    "@babel/plugin-proposal-class-properties",
-    '@babel/plugin-proposal-private-methods',
-    "@babel/plugin-proposal-private-property-in-object",
-    ["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": false }],
-    ["@babel/plugin-proposal-optional-chaining", { "loose": false }],
-    ["@babel/plugin-syntax-dynamic-import", { "loose": false }],
-    ["@babel/plugin-syntax-bigint", { "loose": false }],
-    '@babel/plugin-syntax-jsx'
-  ]
+  presets: [['babel-preset-gatsby']],
+  plugins: ['babel-plugin-styled-components']
 }
